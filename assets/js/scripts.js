d66428cc5155279107cd5da588a3bf10fa4c368f
Johnnies_Ye <492262927@qq.com>
2016-06-14 20:39:45 +0800

    delete projects

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -15,6 +15,11 @@ $(function() {
   $(".content").fitVids();
 });
 
+// delete projects
+$(function(){
+   $(".dl-menu dl-menuopen").lastChild.css("display","none"); 
+});
+
 // All others
 $(document).ready(function() {
     // zoom in/zoom out animations

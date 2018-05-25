737937a94de3056663b6a36a06998ffcd007362c
Berk Özdemir <berkozdemir@Berks-MBP.lan>
2018-05-25 15:15:19 +0200

    js fix

--- cv/sketch.js
+++ cv/sketch.js
@@ -13,87 +13,87 @@ function make2DArray(cols, rows) {
 let grid;
 let cols;
 let rows;
-let resolution = (random(15, 30);
-        let cnv;
+let resolution = (random(15, 30));
+let cnv;
 
 
-        function windowResized() {
-            resizeCanvas(windowWidth, windowHeight);
-            //centerCanvas();
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+    //centerCanvas();
+}
+
+function setup() {
+    frameRate(10);
+    canvas = createCanvas(windowWidth, windowHeight);
+    canvas.position(0, 0);
+    canvas.style('z-index', '-1');
+    // centerCanvas()
+    cols = floor(displayWidth / resolution);
+    rows = floor(displayHeight / resolution);
+
+
+    grid = make2DArray(cols, rows);
+    for (let i = 0; i < cols; i++) {
+        for (let j = 0; j < rows; j++) {
+            grid[i][j] = floor(random(2));
         }
-
-        function setup() {
-            frameRate(10);
-            canvas = createCanvas(windowWidth, windowHeight);
-            canvas.position(0, 0);
-            canvas.style('z-index', '-1');
-            // centerCanvas()
-            cols = floor(displayWidth / resolution);
-            rows = floor(displayHeight / resolution);
+    }
+}
 
 
-            grid = make2DArray(cols, rows);
-            for (let i = 0; i < cols; i++) {
-                for (let j = 0; j < rows; j++) {
-                    grid[i][j] = floor(random(2));
-                }
+
+function draw() {
+    background("#23241f");
+
+    for (let i = 0; i < cols; i++) {
+        for (let j = 0; j < rows; j++) {
+            let x = i * resolution;
+            let y = j * resolution;
+            if (grid[i][j] == 1) {
+                fill(random(255), random(255), random(255));
+                stroke(100);
+                rect(x, y, resolution - 1, resolution - 1);
             }
         }
+    }
 
+    let next = make2DArray(cols, rows);
 
-
-        function draw() {
-            background("#23241f");
-
-            for (let i = 0; i < cols; i++) {
-                for (let j = 0; j < rows; j++) {
-                    let x = i * resolution;
-                    let y = j * resolution;
-                    if (grid[i][j] == 1) {
-                        fill(random(255), random(255), random(255));
-                        stroke(100);
-                        rect(x, y, resolution - 1, resolution - 1);
-                    }
-                }
-            }
-
-            let next = make2DArray(cols, rows);
-
-            // Compute next based on grid
-            for (let i = 0; i < cols; i++) {
-                for (let j = 0; j < rows; j++) {
-                    let state = grid[i][j];
-                    // Count live neighbors!
-                    let sum = 0;
-                    let neighbors = countNeighbors(grid, i, j);
-
-                    if (state == 0 && neighbors == 3) {
-                        next[i][j] = 1;
-                    } else if (state == 1 && (neighbors < 2 || neighbors > 3)) {
-                        next[i][j] = 0;
-                    } else {
-                        next[i][j] = state;
-                    }
-
-                }
-            }
-
-            grid = next;
-
-
-
-        }
-
-
-        function countNeighbors(grid, x, y) {
+    // Compute next based on grid
+    for (let i = 0; i < cols; i++) {
+        for (let j = 0; j < rows; j++) {
+            let state = grid[i][j];
+            // Count live neighbors!
             let sum = 0;
-            for (let i = -1; i < 2; i++) {
-                for (let j = -1; j < 2; j++) {
-                    let col = (x + i + cols) % cols;
-                    let row = (y + j + rows) % rows;
-                    sum += grid[col][row];
-                }
+            let neighbors = countNeighbors(grid, i, j);
+
+            if (state == 0 && neighbors == 3) {
+                next[i][j] = 1;
+            } else if (state == 1 && (neighbors < 2 || neighbors > 3)) {
+                next[i][j] = 0;
+            } else {
+                next[i][j] = state;
             }
-            sum -= grid[x][y];
-            return sum;
+
         }
+    }
+
+    grid = next;
+
+
+
+}
+
+
+function countNeighbors(grid, x, y) {
+    let sum = 0;
+    for (let i = -1; i < 2; i++) {
+        for (let j = -1; j < 2; j++) {
+            let col = (x + i + cols) % cols;
+            let row = (y + j + rows) % rows;
+            sum += grid[col][row];
+        }
+    }
+    sum -= grid[x][y];
+    return sum;
+}
